818142b0ef52f7ac5c20991a7a74c9d59efd1026
Wolfgang Hotwagner <wolfgang.hotwagner@toscom.at>
2016-09-22 19:51:34 +0200

    corrected wrong gem-name in mindwave.rb

--- lib/mindwave.rb
+++ lib/mindwave.rb
@@ -14,7 +14,7 @@
 # GNU General Public License for more details.
 # 
 # You should have received a copy of the GNU General Public License          
-# along with this dokuwiki gem; if not, write to the 
+# along with this mindwave gem; if not, write to the 
 # Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, 
 # Boston, MA  02110-1301  USA 
 #++
